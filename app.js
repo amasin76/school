1e8e79b53df1b47b004ed1110c37b01a3f8c2b4a
amasin76 <51626916+amasin76@users.noreply.github.com>
2022-03-18 14:47:24 +0100

    add nav dropdown list

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ hamburger.addEventListener('click', function () {
     this.classList.toggle("is-active");
     mobileNavBar.classList.toggle("is-active");
 })
+
+const navBarDropList = document.querySelector(".list-drop");
+const plusList = document.querySelector(".plus-list");
+
+plusList.addEventListener('click', function () {
+    navBarDropList.classList.toggle("active");
+})
