fa8e1e4eb0a5d176ac29aef95f829da64a5dfc11
amasin76 <51626916+amasin76@users.noreply.github.com>
2022-03-18 23:24:26 +0100

    add contact section

--- app.js
+++ app.js
@@ -6,9 +6,26 @@ hamburger.addEventListener('click', function () {
     mobileNavBar.classList.toggle("is-active");
 })
 
-const navBarDropList = document.querySelector(".list-drop");
-const plusList = document.querySelector(".plus-list");
+// const navBarDropList = document.querySelector(".list-drop");
+// const plusList = document.querySelector(".plus-list");
 
-plusList.addEventListener('click', function () {
-    navBarDropList.classList.toggle("active");
-})
+// plusList.addEventListener('click', function () {
+//     navBarDropList.classList.toggle("active");
+// })
+
+window.addEventListener('mousedown', function (event) {
+    const navBarDropList = document.querySelector(".list-drop");
+    const plusList = document.querySelector('.plus-list');
+
+    if (event.target === plusList) {
+        navBarDropList.classList.toggle("active")
+
+    } else if (event.target != navBarDropList && event.target.parentNode != navBarDropList) {
+        navBarDropList.classList.remove("active")
+    }
+});
+
+// Update footer date
+const footerDate = document.querySelector("#footer-date")
+const currentYear = new Date().getFullYear()
+footerDate.textContent = currentYear
